e0bb606ee04985788ccca35a20107303987bd53a
Rafael Madakis <rafaelmadakis@gmail.com>
2022-12-31 17:42:49 -0300

    Primeiro teste automatizado

---
+++ Tests/src/main/java/com/example/Tests/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.Tests;
+
+public class Calculator {
+
+  public int sum (int a, int b) {
+    return a + b;
+  }
+
+
+}
---
+++ Tests/src/test/java/com/example/Tests/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.example.Tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+  @Test
+  public void testSum() {
+    Calculator calculator = new Calculator();
+    assertThat(calculator.sum(1,1)).isEqualTo(2);
+
+  }
+
+}
